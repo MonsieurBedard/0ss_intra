81f461e965127616c786b9f996fb40bd9fc2ccec
Alexandre Bédard <1837062@cshawi.ca>
2020-02-24 13:06:21 -0500

    4

---
+++ wpf_intra/intra_models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace intra_models
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactInfo { get; set; }
+        public string PicturePath { get; set; }
+
+        public string Info { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
